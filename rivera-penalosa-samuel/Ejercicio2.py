641ecf304238adc94734f38a6f131c26bab4d2cf
samuelsev <samuelsev26@gmail.com>
2018-04-11 14:01:17 +0200

    subo ejercicio 2

---
+++ rivera-penalosa-samuel/Ejercicio2.py
@@ -0,0 +1,9 @@
+edad=0
+nombre=""
+edad=int(input("Dime tu edad:\n"))
+if edad > 17:
+	nombre=input("cual es tu nombre:\n")
+	print ("hola", nombre)
+else:
+	print ("este programa es para mayores")
+print ("adios")
