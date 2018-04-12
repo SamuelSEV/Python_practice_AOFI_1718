f33c1697f3829d6ec6ef9c98838a91147fcfc466
SamuelSEV <Samuelsev26@gmail.com>
2018-04-12 11:45:34 +0200

    subo ejercicio 8

---
+++ rivera-penalosa-samuel/Ejercicio8.py
@@ -0,0 +1,18 @@
+import time
+comida=0
+dia=0
+Almuerzo={}
+Cena={}
+semana=["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+print ("¿Que comida quiere para esta semana?")
+time.sleep(2)
+for dia in semana:
+		comida=input("¿Que quiere de Almuerzo\n")
+		Almuerzo[dia]=comida
+		comida=input("¿Que quiere de Cena?\n")
+		Cena[dia]=comida
+print("Menu para semana")
+time.sleep(2)
+for dia in semana:
+	print (dia, "-", "Almuerzo:", Almuerzo[dia], "Cena:", Cena[dia])
+	time.sleep(2)
